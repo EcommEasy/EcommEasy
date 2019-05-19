ab159f0866cfb6e1cdce8fcdc5c2bb9b0fbc6f02
SouraVNandA <souravkumarnanda4@gmail.com>
2019-05-19 23:54:42 +0530

    Files Added

---
+++ README.md
@@ -0,0 +1,24 @@
+# E-commAPI - An Ecommerce API
+E-commAPI is Node.js based eCommerce API. 
+
+Built with:
+* Node.js v8.9
+* Express
+* Babel
+* WebPack 4
+* MongoDB
+
+## Installation
+
+
+### Requirements
+* Node.js >= 8
+* MongoDB >= 3.2
+
+
+## Application Structure
+
+
+## Licence
+
+This software is provided free of charge and without restriction under the MIT License
---
+++ src/routes/sitemap.js
@@ -0,0 +1,39 @@
+import security from '../lib/security';
+import SitemapService from '../services/sitemap';
+
+class SitemapRoute {
+	constructor(router) {
+		this.router = router;
+		this.registerRoutes();
+	}
+
+	registerRoutes() {
+		this.router.get(
+			'/v1/sitemap',
+			security.checkUserScope.bind(this, security.scope.READ_SITEMAP),
+			this.getPaths.bind(this)
+		);
+	}
+
+	getPaths(req, res, next) {
+		if (req.query.path) {
+			SitemapService.getSinglePath(req.query.path, req.query.enabled)
+				.then(data => {
+					if (data) {
+						return res.send(data);
+					} else {
+						return res.status(404).end();
+					}
+				})
+				.catch(next);
+		} else {
+			SitemapService.getPaths(req.query.enabled)
+				.then(data => {
+					return res.send(data);
+				})
+				.catch(next);
+		}
+	}
+}
+
+export default SitemapRoute;
